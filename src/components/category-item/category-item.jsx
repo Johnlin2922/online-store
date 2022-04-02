4c564d2e0b5554cee8ef3f1c0fe01cff8d84dd2b
Johnlin2922 <johnlin2922@gmail.com>
2022-04-01 23:32:12 -0500

    create directory with items and styling

---
+++ src/components/category-item/category-item.jsx
@@ -0,0 +1,22 @@
+import "./category-item.styles.scss";
+
+const CategoryItem = (props) => {
+    const title = props.category.title; 
+    const imageUrl = props.category.imageUrl;
+    const url = "src/assets/images/hats.png";
+
+    return (
+        <div className="category-container">
+            <div
+                className="background-image"
+                style={{ backgroundImage: `url(${imageUrl})` }}
+            ></div>
+            <div className="category-body-container">
+                <h2>{title}</h2>
+                <p>Shop Now</p>
+            </div>
+        </div>
+    );
+};
+
+export default CategoryItem;
---
+++ src/components/navigation/directory.jsx
@@ -0,0 +1,49 @@
+import "./directory.styles.scss";
+import CategoryItem from "../category-item/category-item";
+import HatsBackgroundImage from "../../assets/images/hats.png"
+import JacketsBackgroundImage from "../../assets/images/jackets.png";
+import MensBackgroundImage from "../../assets/images/mens.png";
+import WomensBackgroundImage from "../../assets/images/womens.png";
+import SneakersBackgroundImage from "../../assets/images/sneakers.png";
+
+const Directory = () => {
+    const categories = [
+        {
+            id: 1,
+            title: "hats",
+            imageUrl: HatsBackgroundImage,
+        },
+        {
+            id: 2,
+            title: "jackets",
+            imageUrl: JacketsBackgroundImage,
+        },
+        {
+            id: 3,
+            title: "sneakers",
+            imageUrl: SneakersBackgroundImage,
+        },
+        {
+            id: 4,
+            title: "womens",
+            imageUrl: WomensBackgroundImage,
+        },
+        {
+            id: 5,
+            title: "mens",
+            imageUrl: MensBackgroundImage,
+        },
+    ];
+
+    return (
+        <div className="directory-container">
+            {categories.map((category) => {
+                return (
+                    <CategoryItem key={category.id} category={category}/>
+                );
+            })}
+        </div>
+    );
+}
+
+export default Directory;
