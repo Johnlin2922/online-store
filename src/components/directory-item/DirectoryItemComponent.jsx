25a2a99a1defd10a746cf3b12c2e8c68cccaf43b
Johnlin2922 <johnlin2922@gmail.com>
2022-04-12 20:05:39 -0500

    changed from Link component to a useNavigate hook

--- src/components/directory-item/DirectoryItemComponent.jsx
+++ src/components/directory-item/DirectoryItemComponent.jsx
@@ -1,21 +1,26 @@
 import { Link } from "react-router-dom";
 import "./DirectoryItem.styles.scss";
+import { useNavigate } from "react-router-dom";
 
 const CategoryItem = (props) => {
     const title = props.category.title; 
     const imageUrl = props.category.imageUrl;
 
+    const navigate = useNavigate();
+
+    const onNavigateHandler = () => {
+        navigate("/shop/" + title);
+    }
+
     return (
-        <div className="directory-item-container">
+        <div className="directory-item-container" onClick={onNavigateHandler}>
             <div
                 className="background-image"
                 style={{ backgroundImage: `url(${imageUrl})` }}
             ></div>
             <div className="body">
-                <Link to={"/shop/" + title}>
-                    <h2>{title.charAt(0).toUpperCase() + title.slice(1)}</h2>
-                    <p>Shop Now</p>
-                </Link>
+                <h2>{title.charAt(0).toUpperCase() + title.slice(1)}</h2>
+                <p>Shop Now</p>
             </div>
         </div>
     );
