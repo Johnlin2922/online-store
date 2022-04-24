e734373e6b4eb4e1e4b50b936ade010f2bde5e38
Johnlin2922 <johnlin2922@gmail.com>
2022-04-24 18:11:48 -0500

    cleanup

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { PersistGate } from "redux-persist/integration/react"
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            <PersistGate persistor={persistor}>
+            <PersistGate loading={null} persistor={persistor}>
                 <BrowserRouter>
                     <App />
                 </BrowserRouter>
